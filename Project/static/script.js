10250df1664fe2981bf0a5b6a7daaf5546ff888c
bhumika0029 <bhumikagodbole29@gmail.com>
2024-05-08 22:05:18 +0530

    jatin

---
+++ Project/static/script.js
@@ -0,0 +1,58 @@
+var menu =document.querySelector("#nav i")
+var cross =document.querySelector("#full i")
+
+
+var  tl =gsap.timeline()
+
+tl.to("#full",{
+    right:0,
+    duration:0.5
+})
+tl.from("#full a",{
+    x:150,
+    duration:0.6,
+    stagger:0.28,
+    opacity:0
+})
+tl.from("#full i",{
+    opacity:0,
+})
+tl.pause()
+
+menu.addEventListener("click",function(){
+    tl.play()
+})
+cross.addEventListener("click",function(){
+    tl.reverse()
+})
+
+var main =document.querySelector("#main")
+// var cursor =document.querySelector("#cursor")
+var imageDiv = document.querySelector("#full a")
+
+main.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        x:dets.x,
+        y:dets.y,
+        duration:0.6,
+        easing:"back.out",
+       
+    })
+})
+imageDiv.addEventListener("mouseenter",function(){
+    // cursor.innerHTML ="view more"
+    gsap.to("#cursor",{
+        scale:3,
+        backgroundColor:"#ffffff8a",
+
+    })
+})
+
+imageDiv.addEventListener("mouseleave",function(){
+    cursor.innerHTML=""
+    gsap.to("#cursor",{
+        scale:1,
+        backgroundColor:"#fff"
+
+    })
+})
