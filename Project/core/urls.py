e37d26aab405a958ff78938ea5462c5f500ef048
Jatin804 <ljatinl.reddy@gmail.com>
2024-07-06 19:56:05 +0530

    Service page base ready Status | Good

--- Project/core/urls.py
+++ Project/core/urls.py
@@ -5,9 +5,9 @@ from core import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('log_in', views.log_in, name='log_in'),  
+    path('log_in', views.log_in, name='log_in'),
     path('sign_up', views.sign_up, name='sign_up'),
-    path('log_out', views.log_out, name='log_out'),    
-    path('services',views.services, name='services'),
+    path('log_out', views.log_out, name='log_out'),
+    path('services', views.services, name='services'),
     path('about', views.about, name='about'),
 ]
